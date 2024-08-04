4f2fe9d8dff91b5a9b676548dfd516f7b956c2e4
Siddhant Dubey <121477435+Siddhant963@users.noreply.github.com>
2024-08-05 02:43:47 +0530

    Add files via upload
    javascript number and variable

---
+++ conver.js
@@ -0,0 +1,11 @@
+let score ="43";
+console.log(typeof score );  // is type is numbar but when we change it 
+
+let valueNumber = Number (score) // we convrt it into number 
+console.log(typeof valueNumber);  // now it type of number 
+
+
+let islogin = 1
+ let boolcon = Boolean(islogin)
+ console.log(typeof boolcon);
+ 
---
+++ test.js
@@ -0,0 +1,30 @@
+console.log("siddhant");
+const accountID = 22323;
+let emaill = "siddhant0867@gmail.com";
+// var name = "siddhant ";
+password = "siddhant_dubey ";
+
+//  accountID = 24332
+email = "siddhant@353gmail.com";
+console.log(accountID);
+console.log(email);
+
+let name = "sidhant"
+let age = 23
+let islogin = true 
+
+// data type in jaava script 
+// int 
+// bigint 
+// String
+// Boolean
+// null
+// // undefined
+// Symbol => for uniquie 
+// object 
+
+console.log(typeof(islogin));
+console.log(typeof(null));
+console.log(typeof(undefined));
+
+
