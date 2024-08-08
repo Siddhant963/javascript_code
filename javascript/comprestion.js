2640c82e649ce8235bfb588d041174e67670cb97
Siddhant Dubey <121477435+Siddhant963@users.noreply.github.com>
2024-08-08 17:42:14 +0530

    Add files via upload

---
+++ javascript/comprestion.js
@@ -0,0 +1,18 @@
+// comperistion in javascript 
+
+// console.log(3>6);
+// console.log(3>=6);
+// console.log(3<6);
+// console.log(3<=6);
+
+console.log("3" >= 3)
+console.log("7" >= 3)
+console.log(true < 1);
+console.log(true > 1);
+console.log(true == 1);
+console.log(true == "1");
+console.log(true> "1");
+console.log(true < "1");
+console.log(true === "1");
+
+
---
+++ javascript/oprestion.js
@@ -0,0 +1,21 @@
+// opprestion in java script 
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+
+console.log("2" + 3);
+console.log(2 + "3");
+console.log("2" + "3");
+console.log("2" + true);
+console.log(true + 3);
+
+console.log(true + "435");
+// console.log("2" + 3);
+// console.log("2" + 3);
+// console.log("2" + 3);
+
+
+
